e92afcdc7dce99f77b9bfe2ec055daffbcfa114a
HeavyPunk <kirillkuchma8@gmail.com>
2025-10-30 02:17:24 +0500

    feat: working simple event

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,23 @@ use linux_embedded_hal::I2cdev;
 use embedded_hal::i2c::{I2c, SevenBitAddress};
 use uinput::device::Builder;
 use uinput::event::absolute::{Absolute, Position};
+use uinput::event::controller::{Digi, Mouse};
+use uinput::event::Controller;
 use uinput::event::Event::Absolute as AbsEvent;
+use uinput::Event;
 
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut i2c = I2cdev::new("/dev/i2c-1")?;
+    let mut i2c = I2cdev::new("/dev/i2c-4")?;
 
     let mut device = Builder::default()?
         .name("ft6336u-touch")?
+        .event(Event::Controller(Controller::Digi(Digi::Touch)))?
         .event(AbsEvent(Absolute::Position(Position::X)))?
+        .max(480)
         .event(AbsEvent(Absolute::Position(Position::Y)))?
+        .max(320)
         .create()?;
 
     loop {
@@ -25,11 +31,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let x = (((buf[1] & 0x0F) as u16) << 8) | buf[2] as u16;
             let y = (((buf[3] & 0x0F) as u16) << 8) | buf[4] as u16;
 
+            // Landscape mode
+            let (x, y) = (y, 320 - x);
+
             println!("Touch at: x = {x}, y = {y}");
 
+            // device.send(Event::Controller(Controller::Mouse(Mouse::Left)), 1)?;
+            device.send(Event::Controller(Controller::Digi(Digi::Touch)), 1)?;
             device.send(AbsEvent(Absolute::Position(Position::X)), x as i32)?;
             device.send(AbsEvent(Absolute::Position(Position::Y)), y as i32)?;
             device.synchronize()?;
+
+            // thread::sleep(Duration::from_millis(200));
+            
+            // device.send(Event::Controller(Controller::Digi(Digi::Touch)), 1)?;
+            // device.synchronize()?;
+            // thread::sleep(Duration::from_millis(200));
         }
 
         thread::sleep(Duration::from_millis(16));
